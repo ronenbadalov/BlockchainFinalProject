0cfa6d47c1c989008f261541a9fe25e1ec32abbc
Ronen <trr0123@gmail.com>
2022-07-15 16:40:57 +0300

    initialization of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const wait = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      await wait(3000);
+      setIsLoading(false);
+    })();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading && <Loader />}
+
+      {!isLoading && (
+        <>
+          <Legend /> <Map />
+        </>
+      )}
     </div>
   );
 }
