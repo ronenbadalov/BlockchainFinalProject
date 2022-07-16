5a9d4c7c9a9aaf214e2e6d256d84bfec7f8657e7
Ronen <trr0123@gmail.com>
2022-07-16 14:13:14 +0300

    merged branches and updated mapData.json

--- src/components/UI/Land.js
+++ src/components/UI/Land.js
@@ -1,40 +1,19 @@
-import React, { memo, useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { memo, useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Land = (props) => {
-  const [classColor, setClassColor] = useState('');
+  const [classColor, setClassColor] = useState("");
   const currentUser = {};
   useEffect(() => {
     (async () => {
-      if (props.type === 'road_land') return setClassColor('gray');
-      else if (props.type === 'park_land') return setClassColor('green');
-      else if (props.owners[props.id] == 0) return setClassColor('blue');
+      if (props.type === "road_land") return setClassColor("gray");
+      else if (props.type === "park_land") return setClassColor("green");
+      else if (props.owners[props.id] == 0) return setClassColor("blue");
       else if (props.owners[props.id] == props.accounts[0])
-        return setClassColor('yellow');
-      else return setClassColor('red');
+        return setClassColor("yellow");
+      else return setClassColor("red");
     })();
-    // switch (props.type) {
-    //   case 'land':
-    //     if (props.isOcupied) {
-    //       if (props.owner === currentUser.id) {
-    //         if (props.forSale) setClassColor('orange');
-    //         else setClassColor('yellow');
-    //       } else {
-    //         if (props.forSale) setClassColor('purple');
-    //         else setClassColor('red');
-    //       }
-    //     } else {
-    //       setClassColor('blue');
-    //     }
-    //     break;
-    //   case 'road_land':
-    //     setClassColor('gray');
-    //     break;
-    //   case 'park_land':
-    //     setClassColor('green');
-    //     break;
-    // }
   }, []);
 
   const showLandDataInModal = () => {
@@ -64,21 +43,21 @@ const Land = (props) => {
         variant="link"
         className={`rounded-0 p-0 ${classColor}`}
         style={{
-          outline: '1px solid black',
-          boxShadow: 'none',
-          fontSize: '8px',
-          width: '30px',
-          height: '30px',
-          borderColor: 'transparent',
-          color: 'white',
-          textDecoration: 'none',
+          outline: "1px solid black",
+          boxShadow: "none",
+          fontSize: "8px",
+          width: "30px",
+          height: "30px",
+          borderColor: "transparent",
+          color: "white",
+          textDecoration: "none",
         }}
         id={props.id}
         key={props.id}
         disabled={props.disabled}
         onClick={showLandDataInModal}
       >
-        {props.disabled ? '' : props.price}
+        {props.disabled ? "" : props.price}
       </Button>
     </>
   );
